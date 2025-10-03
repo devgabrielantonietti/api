caab1f6829366a97cdc87dd81ea5802c0b7bc4a4
devgabrielantonietti <devgabrielantonietti@gmail.com>
2025-10-03 17:11:29 -0300

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ admin.initializeApp({
     type: process.env.FIREBASE_TYPE,
     project_id: process.env.FIREBASE_PROJECT_ID,
     private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // necessário para que quebras de linha funcionem
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'), // ← essencial
     client_email: process.env.FIREBASE_CLIENT_EMAIL,
     client_id: process.env.FIREBASE_CLIENT_ID,
     auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -50,6 +50,7 @@ admin.initializeApp({
     client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL
   })
 });
+
 
 // ---------- Firestore ----------
 const db = admin.firestore();
